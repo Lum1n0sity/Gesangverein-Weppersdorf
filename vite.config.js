eaacf978f896901e5b2762d96dcb1659afbcb468
Raphael Schuhmann <raphael221@outlook.de>
2025-08-20 20:39:43 +0200

    Made project build ready

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [tailwindcss()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        impressum: resolve(__dirname, 'impressum.html'),
+        datenschutz: resolve(__dirname, 'datenschutz.html')
+      }
+    }
+  }
+});
